0891949bd6007e0f78fe2511c849da0a0177dc63
yssnAIE <yassine.bouazzati@etu.umontpellier.fr>
2023-02-10 15:07:22 +0100

    Ajout des fichiers java et uml des questions 1,2 et 3

---
+++ RENDUS/Java/Exo1/src/main/java/Socket.java
@@ -0,0 +1,29 @@
+public class Socket{
+
+    private String state;
+
+    public Socket(){
+        state = "initial";
+    }
+
+    public void listen() {
+        state = "ecoute";
+    }
+
+    private void read(){
+        System.out.println("message dans la console");
+    }
+
+    public void accept() {
+        state = "connecté";
+        read();
+    }
+
+    public void close() {
+        state = "fermée";
+    }
+
+    public void connect() {
+        state = "connecté";
+    }
+}
